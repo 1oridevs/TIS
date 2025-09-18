58222c78657aa70cfd9aeba6a325adad129a92f5
Ori Cohen <oridevs.offical@gmail.com>
2025-09-18 10:12:36 +0300

    Fix tuple access using correct index in AnalyticsView

--- TIS/Views/AnalyticsView.swift
+++ TIS/Views/AnalyticsView.swift
@@ -333,7 +333,7 @@ struct AnalyticsView: View {
                     .padding()
             } else {
                 ForEach(Array(jobEarnings.enumerated()), id: \.offset) { index, jobEarning in
-                    let (job, earnings) = jobEarning.element
+                    let (job, earnings) = jobEarning.1
                     HStack {
                         Text("\(index + 1).")
                             .font(.subheadline)
