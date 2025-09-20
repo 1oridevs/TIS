0be109dac306b49194e9bfd240bc70082d6123f8
Ori Cohen <oridevs.offical@gmail.com>
2025-09-20 23:21:20 +0300

    Fix ToastType scope by creating separate ToastTypes.swift file

--- TIS/Components/ToastView.swift
+++ TIS/Components/ToastView.swift
@@ -48,30 +48,6 @@ struct ToastView: View {
     }
 }
 
-public enum ToastType {
-    case success
-    case error
-    case warning
-    case info
-    
-    var icon: String {
-        switch self {
-        case .success: return "checkmark.circle.fill"
-        case .error: return "xmark.circle.fill"
-        case .warning: return "exclamationmark.triangle.fill"
-        case .info: return "info.circle.fill"
-        }
-    }
-    
-    var color: Color {
-        switch self {
-        case .success: return TISColors.success
-        case .error: return TISColors.error
-        case .warning: return TISColors.warning
-        case .info: return TISColors.primary
-        }
-    }
-}
 
 struct ToastModifier: ViewModifier {
     @Binding var isShowing: Bool
